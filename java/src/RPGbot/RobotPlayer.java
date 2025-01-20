bdaceacea25a24f24f3acfd506c7857ae0fb40cf
MideCS <mide@mit.edu>
2025-01-19 20:01:05 -0500

    Merged RPGBot with Richard's version
    Apparently Richard had the old RPG bot

--- java/src/RPGbot/RobotPlayer.java
+++ java/src/RPGbot/RobotPlayer.java
@@ -2,36 +2,37 @@ package RPGbot;
 
 import battlecode.common.*;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 
 public class RobotPlayer {
 
     static int turnCount = 0;
+    static int soldierCooldown = 0;
+    static int mopperCooldown = 0;
+    static boolean spawnedMopper = false;
+    static HashMap<MapLocation, Integer> checked = new HashMap<>();
+    static int checked_limit = 1;
 
     static final Random rng = new Random(6147);
 
     static final Direction[] directions = {
-        Direction.NORTH,
-        Direction.NORTHEAST,
-        Direction.EAST,
-        Direction.SOUTHEAST,
-        Direction.SOUTH,
-        Direction.SOUTHWEST,
-        Direction.WEST,
-        Direction.NORTHWEST,
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
     };
 
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
         // Hello world! Standard output is very useful for debugging.
         // Everything you say here will be directly viewable in your terminal when you run a match!
-        System.out.println("I'm alive");
+//        System.out.println("I'm alive");
 
         rc.setIndicatorString("Hello world!");
 
@@ -42,13 +43,13 @@ public class RobotPlayer {
 
             try {
                 switch (rc.getType()){
-                    case SOLDIER: runSoldier(rc); break; 
+                    case SOLDIER: runSoldier(rc); break;
                     case MOPPER: runMopper(rc); break;
-                    case SPLASHER: break; // Consider upgrading examplefuncsplayer to use splashers!
+                    case SPLASHER: runSplasher(rc); break; // Consider upgrading examplefuncsplayer to use splashers!
                     default: runTower(rc); break;
-                    }
                 }
-             catch (GameActionException e) {
+            }
+            catch (GameActionException e) {
                 System.out.println("GameActionException");
                 e.printStackTrace();
 
@@ -66,29 +67,41 @@ public class RobotPlayer {
 
     public static void runTower(RobotController rc) throws GameActionException{
 
+        // TODO: Add saving for Tower when Ruin is reached.
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation nextLoc = rc.getLocation().add(dir);
 
-        int robotType = rng.nextInt(3);
-        if (robotType == 0 && rc.canBuildRobot(UnitType.SOLDIER, nextLoc)){
-            rc.buildRobot(UnitType.SOLDIER, nextLoc);
-            System.out.println("BUILT A SOLDIER");
-        }
-        else if (robotType == 1 && rc.canBuildRobot(UnitType.MOPPER, nextLoc)){
-            rc.buildRobot(UnitType.MOPPER, nextLoc);
-            System.out.println("BUILT A MOPPER");
-        }
-        else if (robotType == 2 && rc.canBuildRobot(UnitType.SPLASHER, nextLoc)){
+//        int robotType = 0;
 
-
-            rc.setIndicatorString("SPLASHER NOT IMPLEMENTED YET");
+        if (soldierCooldown < 30) {
+            if (rc.canBuildRobot(UnitType.SOLDIER, nextLoc)){
+                rc.buildRobot(UnitType.SOLDIER, nextLoc);
+//                System.out.println("BUILT A SOLDIER");
+            }
         }
+        else {
+//            System.out.println("Soldier cooldown exceeded");
+//            robotType = rng.nextInt(1,3);
+            if (rc.canBuildRobot(UnitType.MOPPER, nextLoc) && mopperCooldown <= 1){
+                rc.buildRobot(UnitType.MOPPER, nextLoc);
+                System.out.println("BUILT A MOPPER");
+                mopperCooldown ++;
+            }
+            else if (rc.canBuildRobot(UnitType.SPLASHER, nextLoc)){
+                rc.buildRobot(UnitType.SPLASHER, nextLoc);
+                rc.setIndicatorString("BUILT A SPLASHER");
+                mopperCooldown = 0;
+            }
+        }
+
+        soldierCooldown ++;
+        soldierCooldown %= 40;
 
         // Read incoming messages
-        Message[] messages = rc.readMessages(-1);
-        for (Message m : messages) {
-            System.out.println("Tower received message: '#" + m.getSenderID() + " " + m.getBytes());
-        }
+//        Message[] messages = rc.readMessages(-1);
+//        for (Message m : messages) {
+//            System.out.println("Tower received message: '#" + m.getSenderID() + " " + m.getBytes());
+//        }
 
         // TODO: can we attack other bots?
     }
@@ -96,20 +109,17 @@ public class RobotPlayer {
 
     public static void runSoldier(RobotController rc) throws GameActionException{
 
-        MapInfo[] nearbyTiles = rc.senseNearbyMapInfos();
 
-        // Search for a nearby ruin to complete.
-        MapInfo curRuin = null;
-        for (MapInfo tile : nearbyTiles){
-            if (tile.hasRuin()){
-                curRuin = tile;
-            }
-        }
+        MapInfo curRuin = getNearestRuin(rc);
+        boolean moved = false;
+
         if (curRuin != null){
+            Direction dir = rc.getLocation().directionTo(curRuin.getMapLocation());
             MapLocation targetLoc = curRuin.getMapLocation();
-            Direction dir = rc.getLocation().directionTo(targetLoc);
-            if (rc.canMove(dir))
-                rc.move(dir);
+            moveSoldier(rc, targetLoc);
+
+
+
 
             // Mark the pattern we need to draw to build a tower here if we haven't already.
             MapLocation shouldBeMarked = curRuin.getMapLocation().subtract(dir);
@@ -135,12 +145,16 @@ public class RobotPlayer {
             }
         }
 
+        // TODO: implement strategy for direction
         // Move and attack randomly if no objective.
+
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation nextLoc = rc.getLocation().add(dir);
-        if (rc.canMove(dir)){
+
+        if (rc.canMove(dir) && rc.canAttack(nextLoc)) {
             rc.move(dir);
         }
+
 
         // Try to paint beneath us as we walk to avoid paint penalties.
         // Avoiding wasting paint by re-painting our own tiles.
@@ -156,12 +170,10 @@ public class RobotPlayer {
         // Move and attack randomly.
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation nextLoc = rc.getLocation().add(dir);
-        if (rc.canMove(dir)){
-            rc.move(dir);
-        }
+        moveSoldier(rc, nextLoc);
         if (rc.canMopSwing(dir)){
             rc.mopSwing(dir);
-            System.out.println("Mop Swing! Booyah!");
+//            System.out.println("Mop Swing! Booyah!");
         }
         else if (rc.canAttack(nextLoc)){
             rc.attack(nextLoc);
@@ -169,6 +181,39 @@ public class RobotPlayer {
 
         // We can also move our code into different methods or classes to better organize it!
         updateEnemyRobots(rc);
+    }
+
+    public static void runSplasher(RobotController rc) throws GameActionException {
+        MapInfo[] nearbyTiles = rc.senseNearbyMapInfos();
+        MapInfo curRuin = null;
+        int closestRuin = 1000000;
+        Direction dir = null;
+        for (MapInfo tile : nearbyTiles) {
+            if (tile.hasRuin()) {
+                int dist = rc.getLocation().distanceSquaredTo(tile.getMapLocation());
+
+                if (dist < closestRuin) {
+                    curRuin = tile;
+                    closestRuin = dist;
+
+                    dir = curRuin.getMapLocation().directionTo(rc.getLocation());
+
+                    MapLocation target = curRuin.getMapLocation().subtract(dir);
+
+                    if (rc.canAttack(target)) {
+                        rc.attack(target);
+                        System.out.println("HAHA a Splasher Attacked!!");
+
+                    }
+                }
+            }
+        }
+
+
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+
     }
 
     public static void updateEnemyRobots(RobotController rc) throws GameActionException{
@@ -183,7 +228,7 @@ public class RobotPlayer {
             for (int i = 0; i < enemyRobots.length; i++){
                 enemyLocations[i] = enemyRobots[i].getLocation();
             }
-            
+
             RobotInfo[] allyRobots = rc.senseNearbyRobots(-1, rc.getTeam());
 
             // Occasionally try to tell nearby allies how many enemy robots we see.
@@ -196,5 +241,70 @@ public class RobotPlayer {
             }
         }
     }
-}
 
+    public static MapInfo getNearestRuin(RobotController rc) throws GameActionException {
+        MapInfo[] nearbyTiles = rc.senseNearbyMapInfos();
+        MapInfo curRuin = null;
+        int closestRuin = 1000000;
+        Direction dir = null;
+        for (MapInfo tile : nearbyTiles) {
+            if (tile.hasRuin()) {
+                int dist = rc.getLocation().distanceSquaredTo(tile.getMapLocation());
+
+                if (dist < closestRuin) {
+                    curRuin = tile;
+                    closestRuin = dist;
+                }
+            }
+        }
+        return curRuin;
+    }
+
+    public static void moveSoldier(RobotController rc, MapLocation loc) throws GameActionException {
+        boolean moved = false;
+
+        if (loc != null) {
+            Direction dir = rc.getLocation().directionTo(loc);
+            MapLocation newLoc = rc.getLocation().add(dir);
+            boolean needsPainting = !rc.senseMapInfo(newLoc).getPaint().isAlly();
+
+            if (needsPainting && rc.canMove(dir) && rc.canAttack(newLoc)) {
+                rc.move(dir);
+                checked.put(newLoc, 1);
+                return;
+            } else {
+                for (Direction unit_dir : directions) {
+                    if (rc.canMove(unit_dir) && rc.canAttack(newLoc)) {
+                        needsPainting = !rc.senseMapInfo(rc.getLocation().add(unit_dir)).getPaint().isAlly();
+                        newLoc = rc.getLocation().add(unit_dir);
+                        if (needsPainting) {
+                            rc.move(unit_dir);
+                            checked.put(newLoc, 1);
+                            return;
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+        Direction dir = directions[rng.nextInt(directions.length)];
+        MapLocation newLoc = rc.getLocation().add(dir);
+
+        if (rc.canMove(dir) && !checked.containsKey(newLoc) && rc.canAttack(newLoc)) {
+            checked.put(newLoc, 1);
+            rc.move(dir);
+            return;
+        }
+
+        for (Direction unit_dir : directions) {
+            newLoc = rc.getLocation().add(dir);
+            if (rc.canMove(dir) && rc.canAttack(newLoc)) {
+                rc.move(dir);
+                return;
+            }
+        }
+
+    }
+}
